b8398a65ab44192f10b718a2ad3021d86cae3b92
Rico Sta. Cruz <rico@ambiescent.com>
2012-07-08 00:48:51 -0400

    Build.

--- js/stencil.js
+++ js/stencil.js
@@ -4,7 +4,7 @@ https://github.com/rstacruz/stencil.js
 
 
 (function() {
-  var $find, Listener, uniqCount,
+  var $find, Stencil, uniqCount,
     __slice = [].slice;
 
   uniqCount = 0;
@@ -17,9 +17,9 @@ https://github.com/rstacruz/stencil.js
     }
   };
 
-  Listener = (function() {
+  Stencil = (function() {
 
-    function Listener($el, model, rules) {
+    function Stencil($el, model, rules) {
       this.$el = $el;
       if (rules == null) {
         rules = model;
@@ -27,22 +27,21 @@ https://github.com/rstacruz/stencil.js
       }
       this.model = model;
       this.events = this._flattenRules(rules);
-      this.memoize(this, 'getSingleRunner');
+      this._memoize(this, 'getSingleRunner');
       this.bind();
     }
 
-    Listener.prototype.memoize = function(obj, attr) {
-      var fn, hasher;
+    Stencil.prototype._memoize = function(obj, attr) {
+      var hasher;
       hasher = function(a, b, $el) {
         var uniq, _base, _ref;
         uniq = (_ref = (_base = $el[0]).uniq) != null ? _ref : _base.uniq = uniqCount++;
         return [a, b, uniq];
       };
-      fn = obj[attr];
-      return obj[attr] = _.memoize(fn, hasher);
+      return obj[attr] = _.memoize(obj[attr], hasher);
     };
 
-    Listener.prototype.bind = function() {
+    Stencil.prototype.bind = function() {
       var _ref,
         _this = this;
       if (!((_ref = this.model) != null ? _ref.on : void 0)) {
@@ -61,7 +60,7 @@ https://github.com/rstacruz/stencil.js
       return this;
     };
 
-    Listener.prototype.unbind = function() {
+    Stencil.prototype.unbind = function() {
       var _ref,
         _this = this;
       if (!((_ref = this.model) != null ? _ref.off : void 0)) {
@@ -76,7 +75,7 @@ https://github.com/rstacruz/stencil.js
       return this;
     };
 
-    Listener.prototype._flattenRules = function(rules) {
+    Stencil.prototype._flattenRules = function(rules) {
       var re,
         _this = this;
       re = {};
@@ -94,7 +93,7 @@ https://github.com/rstacruz/stencil.js
       return re;
     };
 
-    Listener.prototype.run = function() {
+    Stencil.prototype.run = function() {
       var args, events;
       events = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
       if (events == null) {
@@ -110,7 +109,7 @@ https://github.com/rstacruz/stencil.js
       return this;
     };
 
-    Listener.prototype.runDirectives = function(directives, args, $el) {
+    Stencil.prototype.runDirectives = function(directives, args, $el) {
       var fn;
       if ($el == null) {
         $el = this.$el;
@@ -119,7 +118,7 @@ https://github.com/rstacruz/stencil.js
       return fn(args);
     };
 
-    Listener.prototype.getRunner = function(directives, $el) {
+    Stencil.prototype.getRunner = function(directives, $el) {
       var runners,
         _this = this;
       if ($el == null) {
@@ -135,7 +134,7 @@ https://github.com/rstacruz/stencil.js
       };
     };
 
-    Listener.prototype.getSingleRunner = function(matcher, handler, $el) {
+    Stencil.prototype.getSingleRunner = function(matcher, handler, $el) {
       var action, m, match, selector;
       match = function(str, regex) {
         var m;
@@ -158,14 +157,14 @@ https://github.com/rstacruz/stencil.js
       return this.runners[action].apply(this, [selector, handler, $el, m[1], m[2], m[3]]);
     };
 
-    Listener.prototype.formats = {
+    Stencil.prototype.formats = {
       selector: /^\s*([^\s]+)\s*(.*?)\s*$/,
       add: /^(.*?)\s*>\s*([^>]+?)$/,
       attr: /^(.*?)\s*@([A-Za-z0-9\-\_]+)$/,
       remove: /^(.*?)\s*>\s*([^>]+?)\s*@([A-Za-z0-9\-\_]+)\s*$/
     };
 
-    Listener.prototype.runners = {
+    Stencil.prototype.runners = {
       "default": function(selector, handler, $el, action) {
         var fn,
           _this = this;
@@ -231,7 +230,7 @@ https://github.com/rstacruz/stencil.js
       }
     };
 
-    Listener.prototype.runGlob = function(glob) {
+    Stencil.prototype.runGlob = function(glob) {
       var events;
       events = _.filter(_.keys(this.events), function(event) {
         return event.match(new RegExp("^" + (glob.replace('*', '.*')) + "$"));
@@ -239,7 +238,7 @@ https://github.com/rstacruz/stencil.js
       return this.run(events);
     };
 
-    Listener.prototype.getDirectivesFor = function(events) {
+    Stencil.prototype.getDirectivesFor = function(events) {
       var directives,
         _this = this;
       if (!_.isArray(events)) {
@@ -254,12 +253,12 @@ https://github.com/rstacruz/stencil.js
       return directives;
     };
 
-    return Listener;
+    return Stencil;
 
   })();
 
   $.fn.stencil = function(model, bindings) {
-    return new Listener(this, model, bindings);
+    return new Stencil(this, model, bindings);
   };
 
 }).call(this);
